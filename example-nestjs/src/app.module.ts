5a7a3fab433cd3172c0c6a94c67ccb77fe4c5c34
Diogo Almazan <diogodpa@outlook.com>
2025-04-02 21:04:05 -0300

    Add TypeORM and MySQL configuration with Docker Compose setup

--- example-nestjs/src/app.module.ts
+++ example-nestjs/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
